1b4a93636b5b449cc2b6723b227c76e0a6b08acb
balanced-anchor-3429 <sahnunhm@gmail.com>
2019-05-11 00:55:35 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -2,6 +2,7 @@
 # Given a hash with numeric values, return the key for the smallest value
 
 def key_for_min_value(name_hash)
+<<<<<<< HEAD
   if name_hash.size == 0 
     return nil 
     else 
@@ -17,4 +18,22 @@ def key_for_min_value(name_hash)
       end
           return min_key
   end
+=======
+
+if name_hash.size == 0
+  return nil 
+  
+
+
+name_hash.collect do |key, value|
+ current_min = ""
+  current_min = key 
+  if(value<name_hash[current_min])
+    current_min = key 
+  end
+end
+  
+  return current_min
+
+>>>>>>> 4bd586f9d65006eec605a43ece2c804f3b78dd0e
 end
